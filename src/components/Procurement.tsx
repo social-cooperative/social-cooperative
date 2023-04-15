69700854cfc083500bf72d2ab60126f2236c1dce
JamesLepage <amvoronkovv@gmail.com>
2023-04-16 00:34:48 +0300

    add reports

--- src/components/Procurement.tsx
+++ src/components/Procurement.tsx
@@ -130,8 +130,6 @@ const ByProducts = ({ orders }) => {
   }</>
 }
 
-const sortByDate = ([ka, a], [kb, b]) => b.date - a.date
-
 export const Orders = ({ historical = false, start = 0, end = Infinity }) => {
   const [byProducts, toggleByProducts] = useToggle(false)
   const [total, count, orders] = useProcurement({ historical, start, end })
@@ -146,73 +144,92 @@ export const Orders = ({ historical = false, start = 0, end = Infinity }) => {
     }))
   }, [orders])
 
-  // TODO: ОТЧЁТЫ
-  console.log('Пономенклатурный отчёт');
-  console.log(JSON.stringify(Object.values(Object.values(orders)
-    .flatMap(item => Object.values(item))
-    .flatMap(({products}) => Object.values(products))
-    .reduce((acc, {count, product} /** [{count, product}]*/) => {
-      if (acc[product.id]) {
-        acc[product.id]['Количество'] += count;
-        acc[product.id]['Сумма'] += count* product.price;
-      } else {
-        acc[product.id] = {
+  const downloadReportByOrders = () => {
+    const data  = generateListFromOrders(orders).sort((a, b) => b.date - a.date).reduce((acc, order, index) => {
+      const weight = Object.values(order.products).reduce((acc: number, {product, count}) => {
+        if (!product.weight) {
+          console.warn(`Для продукта ${product.name} в категории ${product.categoty} не указан вес`);
+        }
+        acc += product.weight ? count * product.weight : 0;
+        return acc;
+      }, 0)
+  
+      acc.push(Object.values(order.products).reduce((accum, {count, product, forChange, forCooperate}) => {
+        accum.push({
+          index,
+          'Номер': order.phone,
+          'Адрес': order.address,
+          'Детали заказа': 
+          `Заказ от ${toLocaleStringRu(order.date)} ${toCurrencyStringRu(productsTotal(order.products))}\n\n`
+          + `${order.name}\n`
+          + `Вес заказа: ${weight}\n кг`
+          + `${order.comment}\n`
+          + `${order.wantToChange ? 'Есть продукты с заменой, в случае недозвона' : ''}
+            ${order.wantToChange ? (order.isRemoveIfNotCalled ? 'удалить их\n\n' : 'заменить их\n\n') : ''}`,
           'Название': product.name,
-          'Категория': product.category,
           'Количество': count,
-          'Слоты':  product.slotCount ?? 1, 
+          'Вес': `${product.weight} кг`,
+          'Слоты': product.slotsCount,
+          'Для замены': forChange ? 'Да' : 'Нет',
+          'Для кооперации': forCooperate ? 'Да' : 'Нет',
           'Цена': product.price,
           'Сумма': product.price * count,
-          'Фасовка': `${product.unit} ${product.unitName}`,
-        }
-      }
+          'Категория': product.category,
+        })
+        return accum;
+      }, []))
       return acc;
-    }, {}))));
+    }, []).flat()
+    const dataToConvert = {
+      data,
+      filename: 'Позаказный отчёт',
+      delimiter: ',',
+      headers: ['Номер', "Адрес", "Детали заказа", "Название", "Количество", "Вес", "Слоты", "Для замены", "Для кооперации", "Цена", "Сумма", "Категория"]
+    }
+    csvDownload(dataToConvert);
+  }
 
-  console.log('Позаказный отчёт');
-  console.log(JSON.stringify(generateListFromOrders(orders).sort((a, b) => b.date - a.date).reduce((acc, order, index) => {
-        const weight = Object.values(order.products).reduce((acc: number, {product, count}) => {
-          if (!product.weight) {
-            console.warn(`Для продукта ${product.name} в категории ${product.categoty} не указан вес`);
+  const downloadReportByProducts = () => {
+    const data = Object.values(Object.values(orders)
+      .flatMap(item => Object.values(item))
+      .flatMap(({products}) => Object.values(products))
+      .reduce((acc, {count, product} /** [{count, product}]*/) => {
+        if (acc[product.id]) {
+          acc[product.id]['Количество'] += count;
+          acc[product.id]['Сумма'] += count* product.price;
+        } else {
+          acc[product.id] = {
+            'Название': product.name,
+            'Категория': product.category,
+            'Количество': count,
+            'Слоты':  product.slotCount ?? 1, 
+            'Цена': product.price,
+            'Сумма': product.price * count,
+            'Фасовка': `${product.unit} ${product.unitName}`,
           }
-          acc += product.weight ? count * product.weight : 0;
-          return acc;
-        }, 0)
+        }
+        return acc;
+      }, {}));
 
+    const dataToConvert = {
+      data,
+      filename: 'Пономенклатурный отчёт',
+      delimiter: ',',
+      headers: ["Название", "Категория", "Количество", "Слоты", "Цена", "Сумма", "Фасовка"]
+    }
+    csvDownload(dataToConvert);
+  }
 
-    acc.push(Object.values(order.products).reduce((accum, {count, product, forChange, forCooperate}) => {
-      accum.push({
-        index,
-        'Номер': order.phone,
-        'Адрес': order.address,
-        'Детали заказа': 
-        `Заказ от ${toLocaleStringRu(order.date)} ${toCurrencyStringRu(productsTotal(order.products))}\n\n`
-        + `${order.name}\n`
-        + `Вес заказа: ${weight}\n кг`
-        + `${order.comment}\n`
-        + `${order.wantToChange ? 'Есть продукты с заменой, в случае недозвона' : ''}
-           ${order.wantToChange ? (order.isRemoveIfNotCalled ? 'удалить их\n\n' : 'заменить их\n\n') : ''}`,
-        'Название': product.name,
-        'Количество': count,
-        'Вес': `${product.weight} кг`,
-        'Слоты': product.slotsCount,
-        'Для замены': forChange ? 'Да' : 'Нет',
-        'Для кооперации': forCooperate ? 'Да' : 'Нет',
-        'Цена': product.price,
-        'Сумма': product.price * count,
-        'Категория': product.category,
-      })
-      return accum;
-    }, []))
-    return acc;
-  }, []).flat()))
-    
   return (
     <Root>
       <PageTitle>
         Закупка
-        <button style={{ float: 'right' }} onClick={toggleByProducts}>{byProducts ? 'По клиентам' : 'По продуктам'}</button>
+        <Button style={{ float: 'right' }} onClick={toggleByProducts}>{byProducts ? 'По клиентам' : 'По продуктам'}</Button>
       </PageTitle>
+      <div>
+        <Button onClick={downloadReportByOrders}>Позаказный отчёт</Button>
+        <Button onClick={downloadReportByProducts}>Пономенклатурный отчёт</Button>
+      </div>
       <Table>
         <thead>
           <tr>
